457b394dd57f53cc099e7bab1e33f2237c7cd599
LaTotty <latotty91@gmail.com>
2025-02-24 13:21:38 +0100

    chore: history ss search config

--- zshrc.d/25-plugins.zsh
+++ zshrc.d/25-plugins.zsh
@@ -5,4 +5,7 @@ znap source zsh-users/zsh-autosuggestions
 
 # Must be loaded before zsh-history-substring-search
 znap source zsh-users/zsh-syntax-highlighting
+
+export HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE="1"
+export HISTORY_SUBSTRING_SEARCH_FUZZY="1"
 znap source zsh-users/zsh-history-substring-search
