0f2c9888fc2d4c394e146d0a01961197ddec58ec
LaTotty <latotty91@gmail.com>
2025-02-27 10:30:57 +0100

    chore: no maintenance for zshstuff

--- zshrc.d/03-zsnap.zsh
+++ zshrc.d/03-zsnap.zsh
@@ -3,9 +3,12 @@
 # Download Znap, if it's not there yet.
 # https://github.com/marlonrichert/zsh-snap
 [[ -r ~/.zshstuff/znap/znap.zsh ]] ||
-    git clone --depth 1 -- \
+    git -c maintenance.auto=false clone --depth 1 -- \
         https://github.com/marlonrichert/zsh-snap.git ~/.zshstuff/znap
 source ~/.zshstuff/znap/znap.zsh
+
+# Avoid automatic git maintenance
+zstyle ':znap:*:*' git-maintenance off
 
 # Auto-Update znap plugins
 ZNAP_UPDATE_FILE="${ZSHSTUFF}/.znap_last_update"
